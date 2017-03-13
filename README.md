372a7a056fa0c94addc6154992db9842c6212ce0
Sebe Keller <sebethkeller@gmail.com>
2017-03-13 16:18:38 -0400

    Extract ints from sentences, add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+#Flip-it
+
+###What does it do?  
+I'm glad you asked! Flip-it takes a file of your choosing and parses out the integers. It then converts those integers to binary and flips all zeros to ones and all ones to zeros before converting it back to an integer. It adds the new and improved sentence back to your file so you can see how things have changed.  
+
+###Ok... But why though?  
+Because it's cool...  
+
+###You've convinced me! How do I run this?  
+1. Make sure you have Python 3 installed  
+2. Go to your terminal and type git clone git@github.com:SebetheWombat/flip-the-bit.git  
+3. Navigate into your fancy new flip-the-bit folder  
+4. Type: python3 flipit.py [/path/to/file.txt]  
+5. Express shock and awe at the results  
--- flipit.py
+++ flipit.py
@@ -1,17 +1,39 @@
+#TODO: pack those bits!
 import sys
+
+#Inform user they did it wrong
+if len(sys.argv) != 2:
+	print("\nDirections:")
+	print("Please make sure to enter the path for the file you wish to change.")
+	print("Use: flipit.py /path/to/file.txt\n")
+	sys.exit()
+
+#Set flipper to file path
 flipper = sys.argv[1]
 
+#Open file for both reading and writing
 f = open(flipper,"r+")
 file_text = f.readlines()
-f.write("\n\nYour bits have been flipped!\n")
 
-def flipping_fantastic(dec_str):
-	for num in dec_str:
-		if num != "\n":
-			b = format(int(num),'b').replace("0","2").replace("1","0").replace("2","1")
-			b = str(int(b,2)) + "\n"
-			f.write(b)
+f.write("\n\nYour bits have been flipped!\n\n")
 
+#0s to 1s, 1s to 0s
+def flipping_fantastic(digit):
+		b = format(int(digit),'b').replace("0","2").replace("1","0").replace("2","1")
+		b = str(int(b,2))
+		return b
 
-flipping_fantastic(file_text)
+#Extract integers from file and send them to have their bits flipped. Re-write them to file.
+for sentence in file_text:
+	flipped = ""
+	for word in sentence.split():
+		if word.isdigit():
+			word = flipping_fantastic(word)
+		flipped += word + " "
+	f.write(flipped + "\n")
+
+#Because it's nice to get feedback
+print("Done! Go check out your file!")
+
+#We're done with you now
 f.close()
